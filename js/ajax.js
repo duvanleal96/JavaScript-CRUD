7222ecf420aebb4a0f90d01896b2ce87a8e401c1
duvanleal96 <duvanleal65@gmail.com>
2022-06-13 20:58:24 -0500

    Creacion codigo html
    se implementa el codigo html , la vista del proyecto

---
+++ js/ajax.js
@@ -0,0 +1,6 @@
+const d = document;
+const table = document.querySelector(".crud-table")
+const form = d.querySelector(".crud-form")
+const title = d.querySelector(".crud-title")
+const template = d.querySelector(".crud-template").content,
+const fragment = d.createDocumentFragment()
